c676eebb05fe0fa7c8d76764fa38de37ce4998fd
awkoo <133107732+awkox@users.noreply.github.com>
2026-09-05 11:17:25 +0000

    libterminal添加maven发布

--- libterminal/build.gradle.kts
+++ libterminal/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidLibrary)
+    id("com.vanniktech.maven.publish") version "0.37.0"
 }
 
 android {
@@ -22,6 +23,41 @@ android {
 
 kotlin {
     jvmToolchain(21)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates("io.github.awkox", "libterminal", "1.0.0")
+
+    pom {
+        name.set("Android Terminal Library")
+        description.set("Android Terminal Library")
+        inceptionYear.set("2026")
+        url.set("https://github.com/awkox/AndroidTerminal")
+
+        licenses {
+            license {
+                name.set("GNU General Public License v3.0 or later")
+                url.set("https://www.gnu.org/licenses/gpl-3.0-standalone.html")
+                comments.set("GPL-3.0-or-later")
+            }
+        }
+        developers {
+            developer {
+                id.set("awkoo")
+                name.set("awkoo")
+                url.set("https://github.com/awkox")
+            }
+        }
+        scm {
+            url.set("https://github.com/awkox/AndroidTerminal/")
+            connection.set("scm:git:git://github.com/awkox/AndroidTerminal.git")
+            developerConnection.set("scm:git:ssh://git@github.com/awkox/AndroidTerminal.git")
+        }
+    }
 }
 
 dependencies {
