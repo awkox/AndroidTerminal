f73ea02d30a936bd685d32dbfd6833874ca41460
awkoo <133107732+awkox@users.noreply.github.com>
2026-09-12 09:37:21 +0000

    v3.2.0

--- libterminal/build.gradle.kts
+++ libterminal/build.gradle.kts
@@ -30,7 +30,7 @@ mavenPublishing {
 
     signAllPublications()
 
-    coordinates("io.github.awkox", "libterminal", "3.1.1")
+    coordinates("io.github.awkox", "libterminal", "3.2.0")
 
     pom {
         name.set("Android Terminal Library")
