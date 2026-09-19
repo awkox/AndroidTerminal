56dc99bb61a2fccc6f6a6c6b75c62ae6bb7091bf
awkoo <133107732+awkox@users.noreply.github.com>
2026-09-19 02:41:24 +0000

    libterminal: 显式声明 kotlinx-coroutines 依赖

--- libterminal/build.gradle.kts
+++ libterminal/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     coreLibraryDesugaring(libs.android.desugar)
     implementation(libs.androidx.core)
     implementation(libs.androidx.appcompat)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
