459017d5b94a6982f9fc54289f42edf3f5782c8f
awkoo <133107732+awkox@users.noreply.github.com>
2026-09-25 00:26:21 +0000

    release: 版本号升至 4.0.0 (versionCode 400)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.awkoo.terminal"
         minSdk = 28
         targetSdk = 28
-        versionCode = 311
-        versionName = "3.1.1"
+        versionCode = 400
+        versionName = "4.0.0"
     }
 
     signingConfigs {
